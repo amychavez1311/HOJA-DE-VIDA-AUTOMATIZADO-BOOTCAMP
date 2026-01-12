d8b09e40c185d4a80a88291f6d26196479b9c9f4
amychavez1311 <amychavez131125@gmail.com>
2026-01-12 04:28:26 -0500

    Feat: auto-create superuser via management command; run in build.sh

--- build.sh
+++ build.sh
@@ -7,4 +7,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
+# Create default superuser if env vars are present
+python manage.py create_default_superuser || true
+
 
